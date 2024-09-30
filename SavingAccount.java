c6c139fc576fc19930c8a59997a5e92ada8ff3e1
Russel <russeltsai@gmail.com>
2024-10-01 01:51:14 +0800

    Added constructor for SavingAccount.java

--- SavingAccount.java
+++ SavingAccount.java
@@ -1,2 +1,8 @@
-public class SavingAccount {
+public class SavingAccount extends Account{
+    private double interestRate;
+
+    public SavingAccount(int accountNumber, int pin, double availableBalance, double totalBalance, double interestRate){
+        super(accountNumber, pin, availableBalance, totalBalance); // Call the parent constructor
+        this.interestRate = interestRate;
+    }
 }
