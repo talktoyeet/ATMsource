816bd656c9e78e97feed0e77937beb388f9d4f82
alanluk2226 <83753838+alanluk2226@users.noreply.github.com>
2024-10-22 16:04:23 +0800

    SavingAccount.java

--- SavingAccount.java
+++ SavingAccount.java
@@ -1,5 +1,5 @@
 public class SavingAccount extends Account{
-    private double interestRate;
+    private static double interestRate;
 
     public SavingAccount(int accountNumber, int pin, double availableBalance, double totalBalance, double interestRate){
         super(accountNumber, pin, availableBalance, totalBalance); // Call the parent constructor
@@ -8,9 +8,30 @@ public class SavingAccount extends Account{
 
     public SavingAccount(int accountNumber, int pin, double availableBalance, double totalBalance){
         super(accountNumber, pin, availableBalance, totalBalance); // Call the parent constructor
-        this.interestRate = 0.5;
+        this.interestRate = 0.5; //defalut value of 0.5% per annum
     }
 
+    @Override
+    public void debit(double amount) {
+        super.availableBalance -= amount;
+        super.totalBalance -= amount;
+    }//minus to the available balance and total balance
+    
+    @Override
+    public void credit( double amount ){ 
+           super.availableBalance += amount; 
+           super.totalBalance += amount; 
+    } // add to the available balance and total balance 
+    
+    public double getavailableBalance(){ 
+        return availableBalance;
+    }
+    
+       public double gettotalBalance(){ 
+        return totalBalance;
+    }
+  
+    
     public double getInterestRate()
     {
       return interestRate;
